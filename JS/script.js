ccc0b3954395690aa9b7ba17f8d1ab0fb3c7181f
ahmdafif30 <ahmadafif5092@gmail.com>
2025-03-26 22:32:23 +0700

    revisi alert JavaScript

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,7 @@ function validasiInput() {
     alert("Input tidak boleh kosong!");
     return false;
   } else if (isNaN(sisi) || sisi <= 0) {
-    alert("Masukkan angka yang valid!");
+    alert("Angka harus diatas 0!");
     return false;
   }
 
