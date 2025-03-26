3d8cc8630e932cc5136b1be3d8e8741f09495bf4
ahmdafif30 <ahmadafif5092@gmail.com>
2025-03-26 22:29:30 +0700

    Revisi Javascript

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,56 @@
-document.getElementById("sisi").addEventListener("input", function() {
-    let sisi = this.value;
-    document.getElementById("tampilkan").innerHTML = `
+function validasiInput() {
+  let sisi = document.getElementById("sisi").value;
+
+  if (sisi === "") {
+    alert("Input tidak boleh kosong!");
+    return false;
+  } else if (isNaN(sisi) || sisi <= 0) {
+    alert("Masukkan angka yang valid!");
+    return false;
+  }
+
+  return true;
+}
+
+document.getElementById("sisi").addEventListener("input", function () {
+  let sisi = this.value;
+  document.getElementById("tampilkan").innerHTML = `
     Diketahui : <br>
     Sisi = ${sisi} cm
     `;
-})
+});
 
-document.getElementById("luas").addEventListener("click", function() {
+document.getElementById("luas").addEventListener("click", function () {
+  if (validasiInput()) {
     let sisi = parseFloat(document.getElementById("sisi").value);
     let jawaban = document.getElementById("jawaban");
 
     jawaban.innerHTML = `
-    Luas = sisi x sisi <br> 
-    Luas = ${sisi} x ${sisi} <br>
-    Luas = ${sisi * sisi} cm2
-    `;
+        Luas = sisi x sisi <br> 
+        Luas = ${sisi} x ${sisi} <br>
+        Luas = ${sisi * sisi} cm<sup>2</sup>
+        `;
     document.getElementById("reset").style.display = "block";
-})
+  }
+});
 
-document.getElementById("keliling").addEventListener("click", function() {
+document.getElementById("keliling").addEventListener("click", function () {
+  if (validasiInput()) {
     let sisi = parseFloat(document.getElementById("sisi").value);
     let jawaban = document.getElementById("jawaban");
 
     jawaban.innerHTML = `
-    Keliling = 4 x sisi  <br> 
-    Keliling = 4 x ${sisi} <br>
-    Keliling = ${sisi * 4} cm
-    `;
+        Keliling = 4 x sisi  <br> 
+        Keliling = 4 x ${sisi} <br>
+        Keliling = ${sisi * 4} cm
+        `;
     document.getElementById("reset").style.display = "block";
-})
+  }
+});
 
-document.getElementById("reset").addEventListener("click", function() {
-    document.getElementById("sisi").value = "";
-    document.getElementById("tampilkan").innerHTML = "";
-    document.getElementById("jawaban").innerHTML = "";
-    document.getElementById("reset").style.display = "none";    
-})        
+document.getElementById("reset").addEventListener("click", function () {
+  document.getElementById("sisi").value = "";
+  document.getElementById("tampilkan").innerHTML = "";
+  document.getElementById("jawaban").innerHTML = "";
+  document.getElementById("reset").style.display = "none";
+});
